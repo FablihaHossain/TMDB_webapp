d8758193fca2fd399eab1c02837257c7f84d96bd
Fablihahossain <fablihahossain27@gmail.com>
2026-08-31 22:58:52 -0400

    creating fetch api call for movies and testing new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import MovieCard from "./components/MovieCard";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  // async function to fetch all movies in first page of TMDB
+  const fetchMovies = async () => {
+    try {
+      const response = await fetch(
+        "https://api.themoviedb.org/3/discover/movie?api_key=62df2cd3a4881de6558bc68cd67cca20",
+      );
+      const data = await response.json();
+      console.log(data);
+      setMovies(data.results);
+    } catch (error) {
+      console.error("Failed to fetch movies... :(", error);
+    } finally {
+      // continue
+    }
+  };
+
+  // fetching movies and setting initial state
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
   return (
     <>
-      <p> test page</p>
+      <h1>Check out these cool movies!</h1>
+      {movies.map((movie) => (
+        <MovieCard
+          title={movie.title}
+          poster_img={movie.poster_path}
+          year={movie.release_date}
+        ></MovieCard>
+      ))}
     </>
   );
 }
